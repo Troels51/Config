921d99e17af2c281b4f40875d10fa72fe1ca028f
Henrik Nymann Jensen <=>
2014-09-29 11:11:19 +0200

    Added config{reader,writer}

---
+++ include/ConfigWriter.hpp
@@ -0,0 +1,17 @@
+#ifndef CONFIGWRITER_H
+#define CONFIGWRITER_H
+
+namespace Config
+{
+    class ConfigWriter 
+    {
+    public:
+        ConfigWriter();
+        ~ConfigWriter();
+                
+    };
+    
+}
+
+
+#endif
